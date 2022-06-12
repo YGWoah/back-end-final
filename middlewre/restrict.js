f729856268be1f2937e7cb39a8ab4c389c0923f7
yg <hmz.elm@outlook.sa>
2022-06-12 22:59:49 +0000

    fixing restrict

--- middlewre/restrict.js
+++ middlewre/restrict.js
@@ -1,12 +1,19 @@
-const restrict = (req,res,next)=>{
-    if(req.session.user){
-        next()
-    }else{
-        res.status(401).json({
-            succes:false,
-            message:"u not logged in"
-        })
+const restrict = (req, res, next) => {
+  try {
+    if (req.session.user) {
+      next();
+    } else {
+      res.status(401).json({
+        succes: false,
+        message: 'u not logged in',
+      });
     }
-}
+  } catch (error) {
+    res.json({
+      succes: false,
+      message: error,
+    });
+  }
+};
 
-module.exports = {restrict}
+module.exports = { restrict };
